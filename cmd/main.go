e19a744d7c5b7b0382b3329225642eb89aaeafcc
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-10 00:47:28 +0300

    Added MySQL database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/DanielJohn17/go-commerce/cmd/api"
+	"github.com/DanielJohn17/go-commerce/cmd/api/db"
+	"github.com/DanielJohn17/go-commerce/config"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
