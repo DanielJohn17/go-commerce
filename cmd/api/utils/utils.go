58efbf83ca7281b92150bc1d283d1f73adfd73ce
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-10 17:47:49 +0300

    functions to parse json, write json and write error

---
+++ cmd/api/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ParseJSON(c *gin.Context, payload any) error {
+	if c.Request.Body == nil {
+		return fmt.Errorf("missing request Body")
+	}
+	return json.NewDecoder(c.Request.Body).Decode(payload)
+}
+
+func WriteJSON(c *gin.Context, status int, v any) {
+	c.IndentedJSON(status, v)
+}
+
+func WriteError(c *gin.Context, status int, err error) {
+	c.AbortWithStatusJSON(status, gin.H{"error": err.Error()})
+}
