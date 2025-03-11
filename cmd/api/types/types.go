3af58ecf86eb938a9311b2294a202b673c18e5e6
DanielJohn17 <danielyohannes38@gmail.com>
2025-03-11 17:00:03 +0300

    Created User struct type

--- cmd/api/types/types.go
+++ cmd/api/types/types.go
@@ -6,3 +6,18 @@ type RegisterUserPayload struct {
 	Email     string `json:"Email"`
 	Password  string `json:"password"`
 }
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Email     string    `json:"Email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"Email"`
+	Password  string `json:"password"`
+}
